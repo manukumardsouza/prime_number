bc08691085e9df463d587adbe9da4b3e5d436452
Manu Kumar <manukumardsouza385@gmail.com>
2026-01-21 21:31:00 +0530

    Add files via upload

---
+++ question27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+    if (n % i == 0)
+    count++;
+    }
+    if (count == 2)
+    printf("Number is Prime");
+    else
+    printf("Number is Not Prime");
+    return 0;
+}
